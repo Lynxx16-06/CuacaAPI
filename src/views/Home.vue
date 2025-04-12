ef62ce864f342ae90c0786a6fab3f601ffd2eaae
Nama Anda <email@anda.com>
2025-04-12 21:03:17 +0700

    Push2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -126,24 +126,24 @@
 
         <main class="text-white">
           <main class="flex mb-3">
-            <div class="backdrop-blur-2xl bg-black rounded-2xl w-50 mx-5 p-6">
+            <div class="backdrop-blur-2xl bg-black rounded-2xl w-full mx-5 p-6">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);"><path d="M9 16a3.001 3.001 0 0 0 6 0c0-.353-.072-.686-.184-1H9.184A2.962 2.962 0 0 0 9 16z"></path><path d="M18 6V4h-3.185A2.995 2.995 0 0 0 12 2c-1.654 0-3 1.346-3 3v5.8A6.027 6.027 0 0 0 6 16c0 3.309 2.691 6 6 6s6-2.691 6-6a6.027 6.027 0 0 0-3-5.2V10h3V8h-3V6h3zm-4.405 6.324A4.033 4.033 0 0 1 16 16c0 2.206-1.794 4-4 4s-4-1.794-4-4c0-1.585.944-3.027 2.405-3.676l.595-.263V5a1 1 0 0 1 2 0v7.061l.595.263z"></path></svg>
               <span class="text-xs">Terasa Seperti</span>
               <p>{{ weather.current.feelslike_c }}°C</p>
             </div>
-            <div class="backdrop-blur-2xl bg-black rounded-2xl w-50 mx-5 p-6">
+            <div class="backdrop-blur-2xl bg-black rounded-2xl w-full mx-5 p-6">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);"><path d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759 4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"></path></svg>
               <span class="text-xs">UV</span>
               <p>{{ weather.current.uv }}</p>
             </div>
           </main>
           <main class="flex">
-            <div class="backdrop-blur-2xl bg-black rounded-2xl w-50 mx-5 p-6">
+            <div class="backdrop-blur-2xl bg-black rounded-2xl w-full mx-5 p-6">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);"><path d="M18.277 8c.347.596.985 1 1.723 1a2 2 0 0 0 0-4c-.738 0-1.376.404-1.723 1H16V4a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H5.723C5.376 5.404 4.738 5 4 5a2 2 0 0 0 0 4c.738 0 1.376-.404 1.723-1H8v.369C5.133 9.84 4.318 12.534 4.091 14H3a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-.877c.197-.959.718-2.406 2.085-3.418A.984.984 0 0 0 9 11h6a.98.98 0 0 0 .792-.419c1.373 1.013 1.895 2.458 2.089 3.419H17a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1h-1.092c-.227-1.466-1.042-4.161-3.908-5.632V8h2.277zM6 18H4v-2h2v2zm14 0h-2v-2h2v2zm-6-9h-4V5h4v4z"></path></svg>
               <span class="text-xs">Tekanan Udara</span>
               <p>{{ weather.current.pressure_mb }}</p>
             </div>
-            <div class="backdrop-blur-2xl bg-black rounded-2xl w-50 mx-5 p-6">
+            <div class="backdrop-blur-2xl bg-black rounded-2xl w-full mx-5 p-6">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="m8 16 5.991-2L16 8l-6 2z"></path></svg>
               <span class="text-xs">Arah Angin</span>
               <p>{{ weather.current.wind_dir }}</p>
